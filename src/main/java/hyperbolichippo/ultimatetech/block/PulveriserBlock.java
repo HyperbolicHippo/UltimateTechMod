1042cddeb15b6d87fd83a7e84006980f24486dc6
HyperbolicHippo <splishsplashno@gmail.com>
2022-12-02 15:46:32 +0000

    Added pulverizer block (currently does nothing)

---
+++ src/main/java/hyperbolichippo/ultimatetech/block/PulveriserBlock.java
@@ -0,0 +1,9 @@
+package hyperbolichippo.ultimatetech.block;
+
+import net.minecraft.block.Block;
+
+public class PulveriserBlock extends Block {
+    public PulveriserBlock(Settings settings) {
+        super(settings);
+    }
+}
