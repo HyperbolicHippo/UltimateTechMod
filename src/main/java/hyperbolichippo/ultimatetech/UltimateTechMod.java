bb73aff654228a6d2881c17532b6ec1280e07e92
HyperbolicHippo <splishsplashno@gmail.com>
2022-12-02 15:47:41 +0000

    Edited creative tab layout

--- src/main/java/hyperbolichippo/ultimatetech/UltimateTechMod.java
+++ src/main/java/hyperbolichippo/ultimatetech/UltimateTechMod.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +14,11 @@ public class UltimateTechMod implements ModInitializer {
     public static final String MOD_ID = "ultimatetech";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
-            new Identifier(MOD_ID, "item_group"), () -> new ItemStack(Items.COD_SPAWN_EGG));
+    public static final ItemGroup MOD_ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "mod_item_group"), () -> new ItemStack(ModItems.WIRE_CUTTERS));
+
+    public static final ItemGroup ELEMENT_ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "element_item_group"), () -> new ItemStack(ModBlocks.HYDROGEN_ATOM.asItem()));
 
     @Override
     public void onInitialize() {
